be66b0dc82a88cd91368b8960cf9edf1922b99b3
ChenBing19980410 <aoker1989@163.com>
2017-01-23 09:06:00 +0800

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[RKEY] INT NOT NULL PRIMARY KEY, 
+    [Number] NCHAR(10) NULL, 
+    [Name] NCHAR(10) NULL, 
+    [Media] NCHAR(10) NULL, 
+    [Asia] NCHAR(10) NULL, 
+    [Euro] NCHAR(10) NULL, 
+    [NewYork] NCHAR(10) NULL
+)
